a73be234c4a2e9b6dc4a89767dbcd3a91dad0d2f
jesus <jesulillo04@gmail.com>
2025-05-11 12:28:21 +0200

    fibonacci

--- src/com/entornos/JesusChavesGajete/algoritmos/Logaritmos.java
+++ src/com/entornos/JesusChavesGajete/algoritmos/Logaritmos.java
@@ -1,5 +1,31 @@
 package com.entornos.JesusChavesGajete.algoritmos;
 
 public abstract class Logaritmos {
+	
+	/****
+	 * La sucecion de Fibonacci es una secuencia de 
+	 * números en la que, después del 0 y del 1, cada número de la serie se obtiene sumando los dos anteriores
+	 * @param numero
+	 * @return  int 
+	 */
+	
+	public int fibonacci(int numero) {
+        if(numero<0) throw new IllegalArgumentException();
+        
+        int a = 0;
+        int b = 1;
+        
+        if(numero == 0) return 0;
+        if(numero == 1) return 1; 
+        
+        int ultimo = 0;
+        
+        for(int i = 2; i <= numero; i++) {
+            ultimo = a+b;
+            a = b;
+            b = ultimo;
+        }
 
+        return b;
+    }
 }
